7eba28001ce13f4c16d0ae4498e5f8809fa807da
MandyY <manoj.arjunan.github@gmail.com>
2024-02-25 18:43:21 +0530

    Initial Commit

---
+++ GroceryShopping/GroceryShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryShoppingApp.swift
+//  GroceryShopping
+//
+//  Created by Manoj Kumar Arjunan on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
